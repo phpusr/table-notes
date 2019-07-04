ca0a762c6610d3c011409a90de867ab13b370406
phpusr <phpusr@gmail.com>
2019-07-05 00:38:22 +0500

    add: tv_series admin

--- tv_series/admin.py
+++ tv_series/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+
+from tv_series.models import Status, Journal
+
+admin.site.register(Status)
+
+
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ['local_name', 'original_name', 'status', 'last_watched_season', 'last_watched_series',
+                    'last_watched_date', 'rating', 'comment']
+    exclude = ['last_watched_date']
+
+
+admin.site.register(Journal, JournalAdmin)
--- tv_series/models.py
+++ tv_series/models.py
@@ -1,1 +1,24 @@
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Journal(models.Model):
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    local_name = models.CharField(max_length=100)
+    original_name = models.CharField(max_length=100)
+    last_watched_season = models.IntegerField()
+    last_watched_series = models.IntegerField()
+    last_watched_date = models.DateField(default=timezone.now)
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.local_name
+
