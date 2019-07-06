0821439b298f2ab4ebe057ad8c033c0195ca3d4b
phpusr <phpusr@gmail.com>
2019-07-06 20:20:35 +0500

    add: Prod settings

---
+++ main/settings_prod.py
@@ -0,0 +1,10 @@
+from .settings_dev import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_prod.sqlite3'),
+    }
+}
