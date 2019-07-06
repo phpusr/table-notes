acad4dbf6f54a6115f196c87e1c3c0afb201d8e8
phpusr <phpusr@gmail.com>
2019-07-07 00:19:54 +0500

    add: whitenoise for static resources

--- main/settings_prod.py
+++ main/settings_prod.py
@@ -2,6 +2,11 @@ from .settings_dev import *
 
 DEBUG = False
 
+# It needs if DEBUG = false
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
