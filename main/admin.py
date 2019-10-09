8f8011d67146f37cfd1f39212d2d2360057d3d49
phpusr <phpusr@gmail.com>
2019-10-09 14:56:00 +0500

    add: Filter objects by owner

--- main/admin.py
+++ main/admin.py
@@ -5,7 +5,22 @@ admin.site.site_header = 'Tabular Notes'
 
 class OwnerAdmin(admin.ModelAdmin):
 
+    list_display = ['name', 'owner']
+    list_filter = ['owner']
+    search_fields = ['name']
     readonly_fields = ['owner']
+
+    def get_queryset(self, request):
+        default_queryset = super().get_queryset(request)
+
+        if request.user.is_superuser or request.path.endswith('/change/'):
+            return default_queryset
+
+        query_owner_id = request.GET.get('owner__id__exact')
+        if query_owner_id:
+            return default_queryset.filter(owner__id=query_owner_id)
+
+        return super().get_queryset(request).filter(owner__id=request.user.id)
 
     def save_model(self, request, obj, form, change):
         if not hasattr(obj, 'owner'):
@@ -21,3 +36,8 @@ class OwnerAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return super().has_delete_permission(request, obj) and self.__check_owner(request, obj)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if not request.user.is_superuser:
+            kwargs["queryset"] = db_field.related_model.objects.filter(owner=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
