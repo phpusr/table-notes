47154115a4f03658e0e32136123189a9b5258638
phpusr <phpusr@gmail.com>
2019-10-09 18:37:39 +0500

    improve: Filter objects

--- main/admin.py
+++ main/admin.py
@@ -6,42 +6,49 @@ admin.site.site_header = 'Tabular Notes'
 class OwnerAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'owner']
-    list_filter = ['owner']
     search_fields = ['name']
     readonly_fields = ['owner']
 
-    def get_queryset(self, request):
-        default_queryset = super().get_queryset(request)
-        has_view_other = self.__has_view_other(request.user)
+    def get_search_results(self, request, queryset, search_term):
+        view_other = self.__has_view_other(request.user) and request.GET.get('owner__id__exact') is not None
 
-        if request.user.is_superuser or (has_view_other and request.path.endswith('/change/')):
-            return default_queryset
+        if not request.user.is_superuser and not view_other:
+            queryset = queryset.filter(owner__id=request.user.id)
 
-        query_owner_id = has_view_other and request.GET.get('owner__id__exact')
-        if query_owner_id:
-            return default_queryset.filter(owner__id=query_owner_id)
-
-        return super().get_queryset(request).filter(owner__id=request.user.id)
+        return super().get_search_results(request, queryset, search_term)
 
     def save_model(self, request, obj, form, change):
         if not hasattr(obj, 'owner'):
             obj.owner = request.user
         super().save_model(request, obj, form, change)
 
+    def has_view_permission(self, request, obj=None):
+        return super().has_view_permission(request, obj) \
+               and (self.__is_owner(request.user, obj) or self.__has_view_other(request.user))
+
     def has_change_permission(self, request, obj=None):
-        return super().has_change_permission(request, obj) and self.__is_owner(request, obj)
+        return super().has_change_permission(request, obj) and self.__is_owner(request.user, obj)
 
     def has_delete_permission(self, request, obj=None):
-        return super().has_delete_permission(request, obj) and self.__is_owner(request, obj)
+        return super().has_delete_permission(request, obj) and self.__is_owner(request.user, obj)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if not request.user.is_superuser:
             kwargs["queryset"] = db_field.related_model.objects.filter(owner=request.user)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
+    def get_list_filter(self, request, obj=None):
+        if 'owner' not in self.list_filter:
+            self.list_filter.append('owner')
+
+        if not self.__has_view_other(request.user):
+            self.list_filter.remove('owner')
+
+        return self.list_filter
+
     @staticmethod
-    def __is_owner(request, obj):
-        return obj is None or obj.owner_id == request.user.id or request.user.is_superuser
+    def __is_owner(user, obj):
+        return obj is None or obj.owner_id == user.id or user.is_superuser
 
     @staticmethod
     def __has_view_other(user):
