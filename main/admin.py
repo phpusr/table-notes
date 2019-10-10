e3ac1262ab54d826d93b91fd1c2479736b91f679
phpusr <phpusr@gmail.com>
2019-10-10 17:34:29 +0500

    change: Friends can see notes

--- main/admin.py
+++ main/admin.py
@@ -22,11 +22,19 @@ class OwnerListFilter(admin.SimpleListFilter):
     parameter_name = 'owner'
 
     def lookups(self, request, model_admin):
-        # TODO change it
-        return [
-            ['phpusr', 'phpusr'],
-            ['user', 'user'],
-        ]
+        def users_to_tuple(users):
+            return [(user.username, user.username) for user in users]
+
+        current_user = request.user
+
+        if current_user.is_superuser:
+            return users_to_tuple(User.objects.all())
+
+        all_friends = list(current_user.friends.all())
+        if len(all_friends) > 0:
+            all_friends.append(current_user)
+
+        return users_to_tuple(all_friends)
 
     def queryset(self, request, queryset):
         if self.value() is not None:
@@ -40,9 +48,10 @@ class OwnerAdmin(admin.ModelAdmin):
     list_filter = []
 
     def get_search_results(self, request, queryset, search_term):
-        view_other = self.__has_view_other(request.user) and request.GET.get('owner') is not None
+        owner_username = request.GET.get('owner')
+        is_owner_friend = request.user.friends.filter(username=owner_username).exists()
 
-        if not request.user.is_superuser and not view_other:
+        if not (request.user.is_superuser or is_owner_friend):
             queryset = queryset.filter(owner__id=request.user.id)
 
         return super().get_search_results(request, queryset, search_term)
@@ -53,14 +62,19 @@ class OwnerAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
     def has_view_permission(self, request, obj=None):
+        if obj is not None:
+            is_owner_friend = request.user.friends.filter(pk=obj.owner_id).exists()
+        else:
+            is_owner_friend = False
+
         return super().has_view_permission(request, obj) \
-               and (self.__is_owner(request.user, obj) or self.__has_view_other(request.user))
+               and (self._is_owner(request.user, obj) or is_owner_friend)
 
     def has_change_permission(self, request, obj=None):
-        return super().has_change_permission(request, obj) and self.__is_owner(request.user, obj)
+        return super().has_change_permission(request, obj) and self._is_owner(request.user, obj)
 
     def has_delete_permission(self, request, obj=None):
-        return super().has_delete_permission(request, obj) and self.__is_owner(request.user, obj)
+        return super().has_delete_permission(request, obj) and self._is_owner(request.user, obj)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if not request.user.is_superuser:
@@ -71,20 +85,8 @@ class OwnerAdmin(admin.ModelAdmin):
         if OwnerListFilter not in self.list_filter:
             self.list_filter.append(OwnerListFilter)
 
-        # TODO
-        # if not self.__has_view_other(request.user):
-        #     self.list_filter.remove(OwnerListFilter)
-
         return self.list_filter
 
     @staticmethod
-    def __is_owner(user, obj):
+    def _is_owner(user, obj):
         return obj is None or obj.owner_id == user.id or user.is_superuser
-
-    @staticmethod
-    def __has_view_other(user):
-        """
-        Check what user may view content of other users
-        :return: If user has group with name "view_other" that return True else False
-        """
-        return user.is_superuser or user.groups.filter(name='view_other').count() > 0
