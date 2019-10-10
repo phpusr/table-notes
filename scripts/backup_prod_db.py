ef8300381a85e92a3ecb38e9ad9b08a0edc7fd72
phpusr <phpusr@gmail.com>
2019-10-10 15:17:40 +0500

    change: Update "backup_prod_db.py"

--- scripts/backup_prod_db.py
+++ scripts/backup_prod_db.py
@@ -9,11 +9,13 @@ Production db backup script
 
 now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 base_dir = os.path.dirname(os.path.abspath(__file__))
-file_name = f'prod_db_{now}'
+# json (default), xml, yaml (needs to install PyYAML)
+dump_format = 'json'
+file_name = f'prod_db_{now}.{dump_format}'
 file_path = os.path.join(base_dir, file_name)
 
-cmd = f'../manage.py dumpdata --indent=2 > {file_path}.json'
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings_prod')
+cmd = f'../manage.py dumpdata --indent=2 --format={dump_format} -o={file_path}'
 os.system(cmd)
 
 print(f'File saved to: {file_path}')
