d8e407b34dcca13af58ca768d696b6c5dba59219
phpusr <phpusr@gmail.com>
2019-10-10 18:05:07 +0500

    fix: Journal model

--- tv_series/models.py
+++ tv_series/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 
 from main.models import OwnerModel
 
@@ -12,15 +11,21 @@ class Status(OwnerModel):
 
 
 class Journal(OwnerModel):
-    status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    local_name = models.CharField(max_length=100, unique=True)
-    original_name = models.CharField(max_length=100, unique=True)
-    last_watched_season = models.IntegerField(null=True)
-    last_watched_series = models.IntegerField(null=True)
-    last_watched_date = models.DateField(null=True, default=timezone.now)
-    rating = models.IntegerField(null=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, blank=True)
+    local_name = models.CharField(max_length=100)
+    original_name = models.CharField(max_length=100)
+    last_watched_season = models.PositiveIntegerField(null=True, blank=True)
+    last_watched_series = models.PositiveIntegerField(null=True, blank=True)
+    last_watched_date = models.DateField(null=True, blank=True)
+    rating = models.PositiveIntegerField(null=True, blank=True)
     comment = models.TextField(blank=True)
 
     def __str__(self):
         return self.local_name
+
+    class Meta:
+        unique_together = [
+            ['owner', 'local_name'],
+            ['owner', 'original_name']
+        ]
 
