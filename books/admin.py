b267ebe11ad41dc232dd488b72be655b13ad8f87
phpusr <phpusr@gmail.com>
2019-10-11 18:56:46 +0500

    change: Improve admin pages

--- books/admin.py
+++ books/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
 
 from main.admin import OwnerAdmin
-from .models import Author, Book, Source, Journal
+from .models import Author, Book, Source, Journal, Genre, Category
 
-admin.site.register(Author, admin.ModelAdmin)
-admin.site.register(Book, admin.ModelAdmin)
-admin.site.register(Source, admin.ModelAdmin)
+admin.site.register(Source, OwnerAdmin)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    autocomplete_fields = ['authors', 'genre', 'category']
 
 
 @admin.register(Journal)
 class JournalAdmin(OwnerAdmin):
-    list_display = ['book', 'add_date']
+    list_display = ['book', 'source', 'add_date', 'start_date', 'end_date', 'days_number', 'pages_number', 'note']
+    autocomplete_fields = ['book', 'source']
