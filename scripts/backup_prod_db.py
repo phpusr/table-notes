54307ae12415919688e364eb7834d2b46ef70420
phpusr <phpusr@gmail.com>
2019-10-14 20:32:39 +0500

    change: Backup DB to Google Drive

--- scripts/backup_prod_db.py
+++ scripts/backup_prod_db.py
@@ -2,20 +2,58 @@
 
 import datetime
 import os
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 
-"""
-Production db backup script 
-"""
 
-now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-base_dir = os.path.dirname(os.path.abspath(__file__))
-# json (default), xml, yaml (needs to install PyYAML)
-dump_format = 'json'
-file_name = f'prod_db_{now}.{dump_format}'
-file_path = os.path.join(base_dir, file_name)
+def backup_db_to_file():
+    """
+    Production db backup script
+    """
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings_prod')
-cmd = f'../manage.py dumpdata --indent=2 --format={dump_format} -o={file_path}'
-os.system(cmd)
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    # json (default), xml, yaml (needs to install PyYAML)
+    dump_format = 'json'
+    file_name = f'prod_db_{now}.{dump_format}'
+    file_path = os.path.join(base_dir, file_name)
 
-print(f'File saved to: {file_path}')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings_prod')
+    cmd = f'../manage.py dumpdata --indent=2 --format={dump_format} -o={file_path}'
+    os.system(cmd)
+
+    print(f'File saved to: {file_path}')
+    return file_path
+
+
+class GoogleDrive:
+
+    def __init__(self):
+        scopes = ['https://www.googleapis.com/auth/drive']
+        service_account_file = 'gdrive_account.json'
+        credentials = service_account.Credentials.from_service_account_file(service_account_file, scopes=scopes)
+        self.service = build('drive', 'v3', credentials=credentials)
+
+    def upload_file(self, file_path):
+        folder_id = '1DyhiFFUgmpnwxunHT5yYbqJAw023kjeW'
+        file_name = file_path[file_path.rfind('/') + 1:]
+        file_metadata = {
+            'name': file_name,
+            'parents': [folder_id]
+        }
+
+        media = MediaFileUpload(file_path, resumable=True)
+        result = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        print(result)
+
+    def print_files(self):
+        result = self.service.files().list(pageSize=10, fields="nextPageToken, files(id, name, mimeType)").execute()
+        print(result)
+
+
+file_path = backup_db_to_file()
+
+g_drive = GoogleDrive()
+g_drive.upload_file(file_path)
+g_drive.print_files()
