a88fdcb5d92c69d72fc1f9f80df1d3acc59e497e
phpusr <phpusr@gmail.com>
2020-03-17 19:29:36 +0500

    TN-6: Moved settings

--- main/settings.py
+++ main/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 VERSION = '1.4'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
