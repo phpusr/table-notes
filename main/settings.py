ed1be15764a37a471e102a2d589aa01b64663dad
phpusr <phpusr@gmail.com>
2020-03-18 12:26:11 +0500

    TN-6: Changed version to 1.5

--- main/settings.py
+++ main/settings.py
@@ -14,7 +14,7 @@ import os
 
 import dj_database_url
 
-VERSION = '1.4'
+VERSION = '1.5'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
