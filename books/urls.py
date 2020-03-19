7c92f928ca5cc30689f9c588b0c8f782a8be1652
phpusr <phpusr@gmail.com>
2020-03-19 13:13:21 +0500

    TN-14: Removed importing code

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,3 @@
-from django.urls import path
-
-from . import views
-
 app_name = 'books'
 urlpatterns = [
-    path('csv-file-import', views.import_from_csv_file, name='csv-file-import')
 ]
--- tv_series/urls.py
+++ tv_series/urls.py
@@ -1,9 +1,3 @@
-from django.urls import path
-
-from . import views
-
 app_name = 'tv_series'
 urlpatterns = [
-    path('db-import', views.import_from_db, name='import_from_db'),
-    path('csv-file-import', views.import_from_csv_file, name='csv-file-import')
 ]
