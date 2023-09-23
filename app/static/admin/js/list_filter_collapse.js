c033684dcaf911a39f115b4c3b325358dcc271c5
phpusr <phpusr@gmail.com>
2023-09-23 22:45:08 +0500

    #424 Fixed showing and hiding filters

--- app/static/admin/js/list_filter_collapse.js
+++ app/static/admin/js/list_filter_collapse.js
@@ -1,12 +1,16 @@
-/*
-* src: http://qaru.site/questions/3688420/trying-to-showhide-the-changelist-filter-in-django-admin
+/**
+ * Add button for show and hide filters in actions menu
+ * src: http://qaru.site/questions/3688420/trying-to-showhide-the-changelist-filter-in-django-admin
 */
 
-(function($) {
-    $(document).ready($ => {
-        $("tr input.action-select").actions()
-        $('<div id="show-filters" style="float: right;"><a href="#">Show filters</a></p>').prependTo('div.actions')
-        $('#changelist-filter h2').html('<a style="color: white;" id="hide-filters" href="#">Filter &rarr;</a>')
+window.onload = () => {
+    (function($) {
+        $(`
+          <div id="show-filters" style="float: right">
+            <a href="#">Show filters</a>
+          </div>
+        `).prependTo('div.actions')
+        $('#changelist-filter h2').html('<a style="color: white;" id="hide-filters" href="#">Filters &rarr;</a>')
 
         $('#show-filters').click(() => {
             $('#changelist-filter').show('fast')
@@ -14,12 +18,12 @@
             $('#show-filters').hide()
         })
 
-        $('#hide-filters').click(() => {
-            $('#changelist-filter').hide('fast')
-            $('#show-filters').show()
-            $('#changelist').removeClass('filtered')
-        })
-
-        $('#hide-filters').click();
-    })
-})($)
+        $('#hide-filters')
+          .click(() => {
+                $('#changelist-filter').hide('fast')
+                $('#show-filters').show()
+                $('#changelist').removeClass('filtered')
+            })
+          .click()
+    })(django.jQuery)
+}
